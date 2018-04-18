f2e97941958d0d96d6d66e2b81bbdc2d3f7e8b94
Đăng Kiệt <dangkiet000@users.noreply.github.com>
2018-04-18 13:55:12 +0700

    Update Flash API

--- MyLib/src/DataFlash.c
+++ MyLib/src/DataFlash.c
@@ -21,7 +21,7 @@
 /**
  * @brief      Program 32-bit data into specified address of flash
  *
- * @param[in]  u32addr  Flash address include APROM, LDROM, Data Flash 
+ * @param[in]  u32addr  Flash address include APROM, LDROM, Data Flash
  *                      and CONFIG
  * @param[in]  u32data  32-bit Data to program
  *
@@ -29,37 +29,37 @@
  *
  * @details    To program word data into Flash include APROM, LDROM, Data Flash
  *             and CONFIG.
- *             The corresponding functions in CONFIG are listed in FMC section 
+ *             The corresponding functions in CONFIG are listed in FMC section
  *             of Technical Reference Manual.
  */
 void FMC_Write(uint32_t u32addr, uint32_t u32data)
 {
   FMC->ISPCMD = FMC_ISPCMD_PROGRAM;   /* Set ISP Command Code */
   /* Set Target ROM Address. The address must be word alignment. */
-  FMC->ISPADR = u32addr;              
+  FMC->ISPADR = u32addr;
   FMC->ISPDAT = u32data;              /* Set Data to Program */
   FMC->ISPTRG = 0x1;                  /* Trigger to start ISP procedure */
   /* To make sure ISP/CPU be Synchronized */
-  __ISB();                            
+  __ISB();
   while(FMC->ISPTRG);                 /* Waiting for ISP Done */
 }
 
 /**
  * @brief       Read 32-bit Data from specified address of flash
  *
- * @param[in]   u32addr  Flash address include APROM, LDROM, Data Flash and 
+ * @param[in]   u32addr  Flash address include APROM, LDROM, Data Flash and
  *              CONFIG
  *
  * @return      The data of specified address
  *
- * @details     To read word data from Flash include APROM, LDROM, Data 
+ * @details     To read word data from Flash include APROM, LDROM, Data
  *              Flash, and CONFIG.
  */
 uint32_t FMC_Read(uint32_t u32addr)
 {
   FMC->ISPCMD = FMC_ISPCMD_READ; /* Set ISP Command Code */
   /* Set Target ROM Address. The address must be word alignment. */
-  FMC->ISPADR = u32addr;         
+  FMC->ISPADR = u32addr;
   FMC->ISPTRG = 0x1;             /* Trigger to start ISP procedure */
   __ISB();                       /* To make sure ISP/CPU be Synchronized */
   while(FMC->ISPTRG);            /* Waiting for ISP Done */
@@ -71,10 +71,10 @@ uint32_t FMC_Read(uint32_t u32addr)
 /**
  * @brief      Flash page erase
  *
- * @param[in]  u32addr  Flash address including APROM, LDROM, Data Flash and 
+ * @param[in]  u32addr  Flash address including APROM, LDROM, Data Flash and
  *                      CONFIG
  *
- * @details    To do flash page erase. The target address could be APROM, 
+ * @details    To do flash page erase. The target address could be APROM,
  *             LDROM, Data Flash, or CONFIG.
  *             The page size is 512 bytes.
  *
@@ -86,10 +86,10 @@ Std_ReturnType FMC_Erase(uint32_t u32addr)
 {
   FMC->ISPCMD = FMC_ISPCMD_PAGE_ERASE; /* Set ISP Command Code */
   /* Set Target ROM Address. The address must be page alignment. */
-  FMC->ISPADR = u32addr;               
+  FMC->ISPADR = u32addr;
   FMC->ISPTRG = 0x1;                   /* Trigger to start ISP procedure */
   /* To make sure ISP/CPU be Synchronized */
-  __ISB();                            
+  __ISB();
   while(FMC->ISPTRG);                  /* Waiting for ISP Done */
 
   /* Check ISPFF flag to know whether erase OK or fail. */
@@ -108,7 +108,7 @@ Std_ReturnType FMC_Erase(uint32_t u32addr)
   *
   * @return   None
   *
-  * @details  This function will clear ISPEN bit of ISPCON to disable 
+  * @details  This function will clear ISPEN bit of ISPCON to disable
   *           ISP function.
   */
 void FMC_Close(void)
@@ -125,10 +125,10 @@ void FMC_Close(void)
   * @return   None
   *
   * @details  ISPEN bit of ISPCON must be set before we can use ISP commands.
-  *           Therefore, To use all FMC function APIs, user needs to call 
+  *           Therefore, To use all FMC function APIs, user needs to call
   *           FMC_Open() first to enable ISP functions.
   *
-  * @note     ISP functions are write-protected. user also needs to unlock it 
+  * @note     ISP functions are write-protected. user also needs to unlock it
   *           by calling SYS_UnlockReg() before using all ISP functions.
   *
   */
@@ -143,145 +143,143 @@ void FMC_Open(void)
 /**
  * @brief      Program 32-bit data into specified address of flash
  *
- * @param[in]  LulPage  Flash address of Data Flash
- *             List values : DATA_FLS_PAGE_ONE
+ * @param[in]  PageID: Flash address of Data Flash
+ *             List values:  DATA_FLS_PAGE_ONE
  *                           ...
  *                           DATA_FLS_PAGE_EIGHT
- * @param[in]  *LpData  32-bit pointer data point to source data array
- * @param[in]  LusLen  lenght of source data array
+ * @param[in]  TargetAddress: target address in PAGE flash memory. This address
+ *             offset will be  added to the PAGE flash memory base address.
+ *                           Min.: 0
+ *                           Max.: FLS_PAGE_DATASIZE - 1
+ * @param[in]  SourceAddressPtr: Pointer to source data buffer.
+ * @param[in]  Lenght: lenght of source data array
  * @retval     E_OK Success
  * @retval     E_NOT_OK Failed
  *
  * @details    Program 32-bit data into specified address of flash
  *             We only have 8 pages (512 bytes) t store data.
  */
-Std_ReturnType DataFlash_Write(uint32_t LulPageAddr, uint32_t *LpData, 
-                               uint16_t LusLen)
+Std_ReturnType DataFlash_Write(Fls_PageType PageID, \
+                         uint32_t TargetAddress, uint32_t *SourceAddressPtr, \
+                         uint16_t Lenght)
 {
-  uint16_t LusDataIndex;
-  
-  if((LulPageAddr < DATA_FLS_PAGE_ONE) ||
-     (LulPageAddr > DATA_FLS_PAGE_EIGHT))
+  uint16_t LusCnt;
+  Fls_DataType LaaPageData[FLS_PAGE_DATASIZE];
+  Fls_AddressType LddPageAddr;
+
+  if(PageID >= FLS_MAX_PAGE_NUMBER)
   {
     return E_NOT_OK;
   }
+
+  if(SourceAddress > (FLS_PAGE_DATASIZE - 1))
+  {
+    return E_NOT_OK;
+  }
+
+  if(Lenght > FLS_PAGE_DATASIZE - SourceAddress)
+  {
+    return E_NOT_OK;
+  }
+
+  LddPageAddr = FLS_GET_PAGE_ADDR(PageID);
+
   /* Disable register write-protection function */
   SYS_UnlockReg();
-  
-  /* Enable FMC ISP functions */ 
-  FMC_Open(); 
+
+  /* Enable FMC ISP functions */
+  FMC_Open();
+
+  /* Read all data of page to backup */
+  for(LusCnt = 0; LusCnt < FLS_PAGE_DATASIZE; LusCnt++)
+  {
+    LaaPageData[LusCnt] = FMC_Read(LddPageAddr + LusCnt*4);
+  }
+
+  /* Write new data to LaaPageData */
+  for(LusCnt = TargetAddress; LusCnt < Lenght; LusCnt++)
+  {
+    LaaPageData[LusCnt] = *SourceAddressPtr;
+    SourceAddressPtr++;
+  }
 
   /* Erase all data in page (512 bytes) */
-  FMC_Erase(LulPageAddr);
-  
-  for(LusDataIndex = 0; LusDataIndex < LusLen; LusDataIndex++)
+  FMC_Erase(LddPageAddr);
+
+  /* Write whole LaaPageData to FLASH PAGE MEMORY */
+  for(LusCnt = 0; LusCnt < FLS_PAGE_DATASIZE; LusCnt++)
   {
-    FMC_Write(LulPageAddr + LusDataIndex*4, *LpData);
-    LpData++;
+    FMC_Write(LddPageAddr + (LusCnt*4), LaaPageData[LusCnt]);
   }
 
   /* Disable FMC ISP function */
   FMC_Close();
-  
+
   /* Lock protected registers */
   SYS_LockReg();
-  
+
   return E_OK;
 }
 
 /**
  * @brief      Read data flash from address of data flash to array
  *
- * @param[in]  LulPage  Flash address of Data Flash
- *             List values : DATA_FLS_PAGE_ONE
+ * @param[in]  PageID: Flash address of Data Flash
+ *             List values:  DATA_FLS_PAGE_ONE
  *                           ...
  *                           DATA_FLS_PAGE_EIGHT
- * @param[in]  *LpData  32-bit pointer data point to source data array
- * @param[in]  LusLen  lenght of source data array
+ * @param[in]  SourceAddress: data point to target data array.
+ *             Range: 0..FLS_PAGE_DATASIZE - 1
+ * @param[out] *TargetAddressPtr: Pointer to target data buffer.
+ * @param[in]  Lenght: lenght of source data array.
+ *             Range: 1..FLS_PAGE_DATASIZE - SourceAddress
  * @retval     E_OK Success
  * @retval     E_NOT_OK Failed
  *
  * @details    Read data flash from address of data flash
  *             We only have 8 pages (512 bytes) t store data.
  */
-Std_ReturnType DataFlash_Read(uint32_t LulPageAddr, uint32_t *LpDesData, 
-                               uint16_t LusLen)
+Std_ReturnType DataFlash_Read(Fls_PageType PageID, \
+                         uint32_t SourceAddress, uint32_t* TargetAddressPtr, \
+                         uint16_t Lenght)
 {
-  uint16_t LusDataIndex;
-  
-  if((LulPageAddr < DATA_FLS_PAGE_ONE) ||
-     (LulPageAddr > DATA_FLS_PAGE_EIGHT))
+  uint16_t LusCnt;
+
+  if(PageID >= FLS_MAX_PAGE_NUMBER)
   {
     return E_NOT_OK;
   }
-  
+
+  if(SourceAddress > (FLS_PAGE_DATASIZE - 1))
+  {
+    return E_NOT_OK;
+  }
+  if(Lenght > FLS_PAGE_DATASIZE - SourceAddress)
+  {
+    return E_NOT_OK;
+  }
+
   /* Disable register write-protection function */
   SYS_UnlockReg();
-  
-  /* Enable FMC ISP functions */ 
-  FMC_Open(); 
-  
-  for(LusDataIndex = 0; LusDataIndex < LusLen; LusDataIndex++)
+
+  /* Enable FMC ISP functions */
+  FMC_Open();
+
+  for(LusCnt = SourceAddress; LusCnt < Lenght; LusCnt++)
   {
-    *LpDesData = FMC_Read(LulPageAddr + LusDataIndex*4);
-    LpDesData++;
+    *TargetAddressPtr = FMC_Read(LulPageAddr + LusCnt*4);
+    TargetAddressPtr++;
   }
 
   /* Disable FMC ISP function */
   FMC_Close();
-  
+
   /* Lock protected registers */
   SYS_LockReg();
-  
+
   return E_OK;
 }
 
-
-
-/**
- * @brief      Reads from flash memory. 
- *
- * @param[in]  SourceAddress:  Source address in flash memory. This address 
- *             offset will be added to the flash memory base address. 
- *                           Min.: 0 
- *                           Max.: FLS_SIZE - 1 
- * @param[in]  Length: Number of bytes to read.
- *                    Min.: 1 
- *                    Max.: FLS_SIZE - SourceAddress.
- * @param[out] TargetAddressPtr:  Pointer to target data buffer.
- * @retval     Std_ReturnType: - E_NOT_OK: read command has been accepted.
- *                             - E_NOT_OK: read command has not been accepted.
- *
- * @details    Reads from flash memory. 
- */
-Std_ReturnType Fls_Read(Fls_AddressType SourceAddress, \
-                        uint8* TargetAddressPtr, Fls_LengthType Length)
-{
-
-}
-
-/**
- * @brief      Writes one or more complete flash pages. 
- *
- * @param[in]  TargetAddress: Target address in flash memory. This address 
- *             offset will be  added to the flash memory base address.  
- *                           Min.: 0 
- *                           Max.: FLS_SIZE - 1 
- * @param[in]  Length: Number of bytes to read.
- *                    Min.: 1 
- *                    Max.: FLS_SIZE - SourceAddress.
- * @param[in]  SourceAddressPtr: Pointer to source data buffer.
- * @param[out]  None
- * @retval     Std_ReturnType: - E_NOT_OK: write command has been accepted.
- *                             - E_NOT_OK: write command has not been accepted.
- *
- * @details    Writes one or more complete flash pages. 
- */
-Std_ReturnType Fls_Write(Fls_AddressType TargetAddress, \
-    const uint8* SourceAddressPtr, Fls_LengthType Length)
-{
-
-}
 /*** (C) COPYRIGHT 2016 DangKiet Technology Corp. ***/
 
 
